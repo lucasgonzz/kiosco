8494524844b5a91df9ac6654c8d2c10e2a4d6c46
Lucas <lucasgonzalez5500@gmail.com>
2019-08-22 18:22:36 -0300

    se termino el nav de resumen ventas

--- resources/views/main/includes/resumen-ventas-nav.blade.php
+++ resources/views/main/includes/resumen-ventas-nav.blade.php
@@ -1,9 +1,9 @@
 <ul class="nav nav-tabs">
     <li class="nav-item">
-        <a class="nav-link active" href="{{ route('resumen-ventas-hoy') }}">Ventas de hoy</a>
+        <a class="nav-link {{ active('resumen-ventas/hoy') }}" href="{{ route('resumen-ventas-hoy') }}">Ventas de hoy</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link" href="{{ route('resumen-ventas-desde-una-fecha') }}">Desde una fecha</a>
+        <a class="nav-link {{ active('resumen-ventas/desde-una-fecha') }}" href="{{ route('resumen-ventas-desde-una-fecha') }}">Desde una fecha</a>
     </li>
     <li class="nav-item">
         <a class="nav-link" href="#">Dias mas vendidos</a>
